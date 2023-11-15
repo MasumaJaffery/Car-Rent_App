2e5b8e7cb2cb1dbf17a1df8d5d804aad08397efe
Najibullahjafari <najib2020202020@gmail.com>
2023-11-15 19:48:15 +0430

    Add itemform

---
+++ src/components/forms/AddItemForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const AddItemForm = () => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add logic to send data to the Rails backend
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="description">Description:</label>
+        <textarea
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
+
+      <button type="submit">Add Item</button>
+    </form>
+  );
+};
+
+export default AddItemForm;
