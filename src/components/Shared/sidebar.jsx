71aa795dc5bd80be64fba420306f237c2c11c5ab
Najibullahjafari <najib2020202020@gmail.com>
2023-11-16 22:53:49 +0430

    Update sidebar

--- src/components/Shared/sidebar.jsx
+++ src/components/Shared/sidebar.jsx
@@ -1,34 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Icons from '../Homepage/icons';
 
 const Sidebar = ({ onSidebarItemClick }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   const handleLogoClick = () => {
     onSidebarItemClick('carousel');
   };
 
+  const handleHamburgerClick = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="sidebar w-[15vw]">
+    <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
       <button type="button" className="logo" onClick={handleLogoClick}>
         <h2 className="logo1">Super Cars</h2>
       </button>
+      <button
+        type="button"
+        className="hamburger-menu"
+        onClick={handleHamburgerClick}
+      >
+        ☰
+      </button>
       <ul
-        className="sidebar-menu flex flex-col items-start"
-        style={{ marginLeft: '2%' }}
+        className={`sidebar-menu flex flex-col items-start ${
+          isSidebarOpen ? 'open' : ''
+        }`}
       >
         <li className="list">
-          <Link to="/">My Reservations</Link>
+          <Link to="/" onClick={handleHamburgerClick}>
+            My Reservations
+          </Link>
         </li>
         <li className="list">
-          <Link to="/reserve">Reserve</Link>
+          <Link to="/reserve" onClick={handleHamburgerClick}>
+            Reserve
+          </Link>
         </li>
         <li className="list">
-          <Link to="/add">Add</Link>
+          <Link to="/add" onClick={handleHamburgerClick}>
+            Add
+          </Link>
         </li>
         <li className="list">
-          <button type="button">Delete</button>
+          <button type="button" onClick={handleHamburgerClick}>
+            Delete
+          </button>
         </li>
       </ul>
       <div className="logos">
