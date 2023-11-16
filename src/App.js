0197c4477869cc85bd4a81184e9019a649c7ed35
Najibullahjafari <najib2020202020@gmail.com>
2023-11-16 22:13:47 +0430

    Update app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddItemForm from './components/forms/AddItemForm';
 import ReserveAppointmentForm from './components/forms/ReserveAppointmentForm';
 import Home from './components/Homepage/homepage';
+import Sidebar from './components/Shared/sidebar';
+import CarDetailsPage from './components/CarDetails/cardetailspage';
 
 const App = () => (
-  <div>
-    <Home />
-    <AddItemForm />
-    <ReserveAppointmentForm />
-    {/* Add other components as needed */}
-  </div>
+  <Router>
+    <div className="App flex">
+      <Routes>
+        <Route
+          path="/*"
+          element={
+            <>
+              <Sidebar />
+              <div className="content">
+                <Routes>
+                  <Route index element={<Home />} />
+                  <Route path="/add" element={<AddItemForm />} />
+                  <Route path="/reserve" element={<ReserveAppointmentForm />} />
+                  <Route path="/car/:id" element={<CarDetailsPage />} />
+                  {/* Add other routes as needed */}
+                </Routes>
+              </div>
+            </>
+          }
+        />
+      </Routes>
+    </div>
+  </Router>
 );
 
 export default App;
