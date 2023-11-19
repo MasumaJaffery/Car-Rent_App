89e9c149f997b35c8158b3eb94a6c8ef52822815
Najibullahjafari <najib2020202020@gmail.com>
2023-11-19 06:21:52 +0430

    add style for sidebar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignInForm from './components/Deviseforms/signin';
 import SignUpForm from './components/Deviseforms/signup';
-import Homepage from './components/Homepage/homepage';
 import Splash from './components/Shared/splash';
 import AddItemForm from './components/forms/AddItemForm';
 import Home from './components/Homepage/homepage';
@@ -10,15 +9,13 @@ import Sidebar from './components/Shared/sidebar';
 import CarDetailsPage from './components/CarDetails/cardetailspage';
 import ReserveAppointmentForm from './components/forms/ReserveAppointmentForm';
 
-
 const App = () => (
   <Router>
     <div className="App flex">
       <Routes>
-
         <Route path="/login" element={<SignInForm />} />
         <Route path="/signup" element={<SignUpForm />} />
-        <Route path="/Homepage" element={<Homepage />} />
+        <Route path="/Homepage" element={<Home />} />
         <Route path="/" element={<Splash />} />
 
         <Route
