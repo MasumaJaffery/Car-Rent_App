3031ccf0cea42d14f97d924026d81e80dd3be5b7
Najibullahjafari <najib2020202020@gmail.com>
2023-11-20 19:55:32 +0430

    Update form

--- src/components/forms/AddItemForm.jsx
+++ src/components/forms/AddItemForm.jsx
@@ -1,60 +1,56 @@
-import React, { useState } from 'react';
+// CarSellingForm.js
+
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import { useAuth } from '../Shared/AuthContext'; // Adjust the import path
 
 const CarSellingForm = () => {
-  const auth = useAuth();
-  const { user } = auth;
-  const dispatch = useDispatch();
-  if (!user) {
-    // Handle the case when the user is not authenticated
-    console.log('no userrrrrrrrrrrrrr!!');
-    return null;
-  }
-  let initialName = '';
-  let initialDescription = '';
-  let initialCategory = '';
-  let initialAddedBy = '';
+  const [formData, setFormData] = useState(() => {
+    const savedFormData = localStorage.getItem('carFormData');
+    return savedFormData
+      ? JSON.parse(savedFormData)
+      : {
+          name: '',
+          description: '',
+          price: '',
+          city: '',
+          image: '',
+        };
+  });
 
-  if (user) {
-    initialName = user.name;
-    initialDescription = user.description;
-    initialCategory = user.category;
-    initialAddedBy = user.addedBy;
-  }
+  useEffect(() => {
+    localStorage.setItem('carFormData', JSON.stringify(formData));
+  }, [formData]);
 
-  const [name, setName] = useState(initialName);
-  const [description, setDescription] = useState(initialDescription);
-  const [category, setCategory] = useState(initialCategory);
-  const [addedBy, setAddedBy] = useState(initialAddedBy);
-  const [selectedFile, setSelectedFile] = useState(null);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setFormData({
+      ...formData,
+      image: file,
+    });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    const formDataToSend = new FormData();
+    formDataToSend.append('name', formData.name);
+    formDataToSend.append('description', formData.description);
+    formDataToSend.append('price', formData.price);
+    formDataToSend.append('city', formData.city);
+    formDataToSend.append('image', formData.image);
+
     try {
-      const formData = new FormData();
-      formData.append('name', name);
-      formData.append('description', description);
-      formData.append('category', category);
-      formData.append('added_by', addedBy);
-      if (selectedFile) {
-        formData.append('picture', selectedFile, selectedFile.name);
-      }
-
-      if (!user || !user.id) {
-        // Handle the case when the user or user.id is null or undefined
-        console.error('User or user ID is missing.');
-        // You can redirect to a login page or show a message to the user
-        // For example, you might want to redirect to the login page:
-        // history.push('/login');
-        return;
-      }
-
       const response = await axios.post(
-        `http://localhost:4000/api/v1/users/${user.id}/cars`,
-        formData,
+        'http://localhost:4000/api/v1/items',
+        formDataToSend,
         {
           headers: {
             'Content-Type': 'multipart/form-data',
@@ -62,19 +58,18 @@ const CarSellingForm = () => {
         }
       );
 
-      if (response && response.data) {
-        console.log('Car added successfully:', response.data);
-      } else {
-        console.log('Car added successfully, but response data is undefined.');
-      }
+      console.log('Item created:', response.data);
+      setFormData({
+        name: '',
+        description: '',
+        price: '',
+        city: '',
+        image: null,
+      });
+      localStorage.removeItem('carFormData');
     } catch (error) {
-      console.error('Error adding car:', error.response?.data || error.message);
+      console.error('Error creating item:', error);
     }
-  };
-
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    setSelectedFile(file);
   };
 
   return (
@@ -82,80 +77,67 @@ const CarSellingForm = () => {
       onSubmit={handleSubmit}
       className="max-w-md mx-auto mt-10 p-4 bg-white shadow-md rounded-md"
     >
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-600">Name:</label>
+      <label className="block mb-2">
+        Name:
         <input
           type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
           className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-lime-500"
         />
-      </div>
+      </label>
 
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-600">
-          Description:
-        </label>
+      <label className="block mb-2">
+        Description:
+        <textarea
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+          className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-lime-500"
+        />
+      </label>
+
+      <label className="block mb-2">
+        Price:
         <input
           type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          name="price"
+          value={formData.price}
+          onChange={handleChange}
           className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-lime-500"
         />
-      </div>
+      </label>
 
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-600">
-          Category:
-        </label>
+      <label className="block mb-2">
+        City:
         <input
           type="text"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
+          name="city"
+          value={formData.city}
+          onChange={handleChange}
           className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-lime-500"
         />
-      </div>
+      </label>
 
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-600">
-          Added by:
-        </label>
-        <input
-          type="text"
-          value={addedBy}
-          onChange={(e) => setAddedBy(e.target.value)}
-          className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-lime-500"
-        />
-      </div>
-
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-600">
-          Upload Picture:
-        </label>
+      <label className="block mb-2">
+        Upload Picture:
         <input
           type="file"
+          name="image"
           onChange={handleFileChange}
           className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-lime-500"
         />
-        {selectedFile && (
-          <p className="mt-2 text-sm text-gray-500">
-            Selected File: {selectedFile.name}
-          </p>
-        )}
-      </div>
+      </label>
 
       <button
         type="submit"
-        className="w-full bg-lime-500 text-white py-2 px-4 rounded-md hover:bg-lime-700 focus:outline-none"
+        className="w-full bg-lime-500 text-white py-2 px-4 add_item_submit_btn rounded-md hover:bg-lime-700 focus:outline-none"
       >
         Submit
       </button>
     </form>
   );
 };
-
-// CarSellingForm.propTypes = {
-//   // Add any prop types if needed
-// };
 
 export default CarSellingForm;
