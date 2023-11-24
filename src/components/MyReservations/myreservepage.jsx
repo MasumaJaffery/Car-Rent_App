1e8b7ae2f8f25eabb78884f5cabc812be49f090f
MasumaJaffery <masumajaffery1@gmail.com>
2023-11-25 01:45:53 +0500

    Color

--- src/components/MyReservations/myreservepage.jsx
+++ src/components/MyReservations/myreservepage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import MyReserveComponent from './myreservecomponent';
 
-const MyReservepage = () => {
-  return (
-    <div className="App flex">
-      <MyReserveComponent />
-    </div>
-  );
-}
+const MyReservepage = () => (
+  <div className="App flex">
+    <MyReserveComponent />
+  </div>
+);
 
 export default MyReservepage;
