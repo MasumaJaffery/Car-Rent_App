aefac4cf695aaf3e9e50f0bc3b6f28c4b7f90b25
Najibullahjafari <najib2020202020@gmail.com>
2023-11-25 09:38:53 +0430

    Update cardetail page

--- src/components/CarDetails/cardetailspage.jsx
+++ src/components/CarDetails/cardetailspage.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import CarDetails from './CarDetails';
 
-function CarDetailsPage() {
-  return (
-    <CarDetails />
-  );
-}
+const CarDetailsPage = () => <CarDetails />;
 
 export default CarDetailsPage;
